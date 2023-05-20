0f1b682e57b6fa118f7303558589cc51df1fb19f
JESUS ALBERTO LOPEZ ALAY <jlopeza69@miumg.edu.gt>
2023-05-20 08:59:18 -0600

    .

---
+++ src/main/java/com/clase/hashtables/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.clase.hashtables;
+
+/**
+ *
+ * @author Jesus Alberto
+ */
+public class Main {
+    
+    //Crear una clase
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this.hashMethod(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("Maria", 1979);
+myHashTable.set("Donald", 1990);
+myHashTable.set("Emily", 2000);
+}
